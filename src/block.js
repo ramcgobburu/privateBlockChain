9bb28d79e099b8d506d669285dde788b128ffd89
Ram Gobburu <ramgobburu@rams-mbp.lan>
2021-05-28 17:53:34 -0500

    first commit

---
+++ src/block.js
@@ -0,0 +1,88 @@
+/**
+ *                          Block class
+ *  The Block class is a main component into any Blockchain platform, 
+ *  it will store the data and act as a dataset for your application.
+ *  The class will expose a method to validate the data... The body of
+ *  the block will contain an Object that contain the data to be stored,
+ *  the data should be stored encoded.
+ *  All the exposed methods should return a Promise to allow all the methods 
+ *  run asynchronous.
+ */
+
+const SHA256 = require('crypto-js/sha256');
+const hex2ascii = require('hex2ascii');
+
+class Block {
+
+    // Constructor - argument data will be the object containing the transaction data
+	constructor(data){
+		this.hash = null;                                           // Hash of the block
+		this.height = 0;                                            // Block Height (consecutive number of each block)
+		this.body = Buffer(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+		this.time = 0;                                              // Timestamp for the Block creation
+		this.previousBlockHash = null;                              // Reference to the previous Block Hash
+    }
+    
+    /**
+     *  validate() method will validate if the block has been tampered or not.
+     *  Been tampered means that someone from outside the application tried to change
+     *  values in the block data as a consecuence the hash of the block should be different.
+     *  Steps:
+     *  1. Return a new promise to allow the method be called asynchronous.
+     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
+     *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
+     *  4. Compare if the auxiliary hash value is different from the calculated one.
+     *  5. Resolve true or false depending if it is valid or not.
+     *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
+     */
+    validate() {
+        let self = this;
+        let hash = SHA256(JSON.stringify(self));
+        return new Promise((resolve, reject) => {
+                let hash1 = SHA256(hash).toString();
+            // Save in auxiliary variable the current block hash
+               if(hash1==hash){
+               resolve(true)
+                }else{
+                reject(false)
+                }
+
+            // Recalculate the hash of the Block
+            // Comparing if the hashes changed
+            // Returning the Block is not valid
+            
+            // Returning the Block is valid
+
+        });
+    }
+    getBData() {
+    let self = this;
+    const decodedData = hex2ascii(self.body);
+
+            // Parse the data to an object to be retrieve.
+            const block = JSON.parse(decodedData);
+    return new Promise((resolve,reject) =>{
+
+        let deco = JSON.parse(hex2ascii(self.body));
+        if(block.data == 'Genesis Block'){
+        console.log('its a genesis block')
+        reject('rejected the block');
+        }else{
+        console.log("resolved block");
+        resolve(block);
+        }
+
+
+
+        });
+        // Getting the encoded data saved in the Block
+        // Decoding the data to retrieve the JSON representation of the object
+        // Parse the data to an object to be retrieve.
+
+        // Resolve with the data if the object isn't the Genesis block
+
+    }
+
+}
+
+module.exports.Block = Block;                    // Exposing the Block class as a module
